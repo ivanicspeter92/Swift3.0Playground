72539ddda6da35b7083d31168ea14bebda67144d
Peter Ivanics <peter.ivanics@reslink.fi>
2016-06-17 11:20:40 +0300

    Added ColorLiterals.playground

---
+++ Playgrounds/ColorLiterals.playground/Contents.swift
@@ -0,0 +1,5 @@
+// Shows how to use Swift Color Literals. Simply type color and use the intellisense!
+import UIKit
+
+let blue = #colorLiteral(red: 0, green: 0, blue: 1, alpha: 1);
+let white = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1);
