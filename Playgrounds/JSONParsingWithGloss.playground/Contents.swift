634d8f713e37e0350b5c0ec65ddb56bf7f803d65
Peter Ivanics <peter.ivanics@reslink.fi>
2016-06-29 18:46:09 +0300

    Refactored step 1

--- Playgrounds/JSONParsingWithGloss.playground/Contents.swift
+++ Playgrounds/JSONParsingWithGloss.playground/Contents.swift
@@ -5,33 +5,36 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 typealias Payload = [String: AnyObject]
 
-DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
-    var json: Payload!
-    // 1
+func getJsonContents(from data: Data) -> Payload? {
     do {
-        json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions()) as? Payload
+        let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions()) as? Payload
+        return json;
     } catch {
         print(error)
+        return nil;
+    }
+}
+
+DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
+    if let json = getJsonContents(from: data) {
+        guard let feed = json["feed"] as? Payload,
+            let apps = feed["entry"] as? [AnyObject],
+            let app = apps.first as? Payload
+            else { PlaygroundPage.current.finishExecution() }
+        
+        guard let container = app["im:name"] as? Payload,
+            let name = container["label"] as? String
+            else { PlaygroundPage.current.finishExecution() }
+        
+        guard let id = app["id"] as? Payload,
+            let link = id["label"] as? String
+            else { PlaygroundPage.current.finishExecution() }
+        
+        // 3
+        let entry = App(name: name, link: link)
+        print(entry)
+        
         PlaygroundPage.current.finishExecution()
     }
-    
-    // 2
-    guard let feed = json["feed"] as? Payload,
-        let apps = feed["entry"] as? [AnyObject],
-        let app = apps.first as? Payload
-        else { PlaygroundPage.current.finishExecution() }
-    
-    guard let container = app["im:name"] as? Payload,
-        let name = container["label"] as? String
-        else { PlaygroundPage.current.finishExecution() }
-    
-    guard let id = app["id"] as? Payload,
-        let link = id["label"] as? String
-        else { PlaygroundPage.current.finishExecution() }
-    
-    // 3
-    let entry = App(name: name, link: link)
-    print(entry)
-    
-    PlaygroundPage.current.finishExecution()
 }
+
