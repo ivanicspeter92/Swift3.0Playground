997d3ac13d60c5d2d41df627d4d33b06750bcb1f
Peter Ivanics <peter.ivanics@reslink.fi>
2016-06-29 18:43:24 +0300

    Added native swift parsing mechanism

--- Playgrounds/JSONParsingWithGloss.playground/Contents.swift
+++ Playgrounds/JSONParsingWithGloss.playground/Contents.swift
@@ -1,11 +1,37 @@
 //: Playground - noun: a place where people can play
-
-import UIKit
+import Foundation
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
+typealias Payload = [String: AnyObject]
 
 DataManager.getTopAppsDataFromFileWithSuccess { (data) -> Void in
-  // TODO: Process data
-  PlaygroundPage.current.finishExecution()
+    var json: Payload!
+    // 1
+    do {
+        json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions()) as? Payload
+    } catch {
+        print(error)
+        PlaygroundPage.current.finishExecution()
+    }
+    
+    // 2
+    guard let feed = json["feed"] as? Payload,
+        let apps = feed["entry"] as? [AnyObject],
+        let app = apps.first as? Payload
+        else { PlaygroundPage.current.finishExecution() }
+    
+    guard let container = app["im:name"] as? Payload,
+        let name = container["label"] as? String
+        else { PlaygroundPage.current.finishExecution() }
+    
+    guard let id = app["id"] as? Payload,
+        let link = id["label"] as? String
+        else { PlaygroundPage.current.finishExecution() }
+    
+    // 3
+    let entry = App(name: name, link: link)
+    print(entry)
+    
+    PlaygroundPage.current.finishExecution()
 }
