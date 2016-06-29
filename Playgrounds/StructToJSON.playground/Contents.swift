8f35ee3e13782b9ef80dbb79ba37c346468e47a8
Peter Ivanics <peter.ivanics@reslink.fi>
2016-06-29 15:48:21 +0300

    Added source comment

--- Playgrounds/StructToJSON.playground/Contents.swift
+++ Playgrounds/StructToJSON.playground/Contents.swift
@@ -1,3 +1,5 @@
+// http://codelle.com/blog/2016/5/an-easy-way-to-convert-swift-structs-to-json/
+
 let car = Car(manufacturer: "Tesla", model: "Model T", mileage: 1234.56, owner: Owner(name: "Emil"))
 car.manufacturer
 
