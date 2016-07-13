af8693f684abfabede5dd7aa3e2d7a7e443afa62
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-13 15:57:32 +0300

    Removed XCTest import

--- Playgrounds/GeneralPurposeTree.playground/Contents.swift
+++ Playgrounds/GeneralPurposeTree.playground/Contents.swift
@@ -1,8 +1,6 @@
 // Playground for implementing a general purpose tree data structure
 // Source: https://www.raywenderlich.com/138190/swift-algorithm-club-swift-tree-data-structure
 
-import XCTest
-
 let beverages = Node(value: "beverages");
 
 let hotBeverages = Node(value: "hot");
