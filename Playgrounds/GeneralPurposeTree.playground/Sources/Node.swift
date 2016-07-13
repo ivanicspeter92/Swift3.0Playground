ca4709ad5a72827b2bcdb73dd8de5011e5b27c08
Peter Ivanics <peter.ivanics@reslink.fi>
2016-07-13 15:44:11 +0300

    Added basic implementation of the Node class

--- Playgrounds/GeneralPurposeTree.playground/Sources/Node.swift
+++ Playgrounds/GeneralPurposeTree.playground/Sources/Node.swift
@@ -1,3 +1,17 @@
-class Node {
+public class Node {
+    // MARK: - Variables
+    public private(set) var value: String;
+    public private(set) var children = [Node]();
+    public private(set) var parent: Node?;
     
+    // MARK: - Initializers
+    public init(value: String) {
+        self.value = value;
+    }
+    
+    // MARK: - Functions
+    public func addChild(node: Node) {
+        self.children.append(node);
+        node.parent = self;
+    }
 }
