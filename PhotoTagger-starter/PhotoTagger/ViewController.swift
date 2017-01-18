0ceaa21316c2501382ef4caf9885b2e25b867e20
Peter Ivanics <peter.ivanics@reslink.fi>
2017-01-18 22:38:08 +0200

    Completed the first part of the tutorial

--- PhotoTagger-starter/PhotoTagger/ViewController.swift
+++ PhotoTagger-starter/PhotoTagger/ViewController.swift
@@ -21,6 +21,7 @@
  */
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -87,6 +88,31 @@ extension ViewController: UIImagePickerControllerDelegate {
     }
 
     imageView.image = image
+    
+    // 1
+    takePictureButton.isHidden = true
+    progressView.progress = 0.0
+    progressView.isHidden = false
+    activityIndicatorView.startAnimating()
+    
+    upload(
+      image: image,
+      progressCompletion: { [unowned self] percent in
+        // 2
+        self.progressView.setProgress(percent, animated: true)
+      },
+      completion: { [unowned self] tags, colors in
+        // 3
+        self.takePictureButton.isHidden = false
+        self.progressView.isHidden = true
+        self.activityIndicatorView.stopAnimating()
+        
+        self.tags = tags
+        self.colors = colors
+        
+        // 4
+        self.performSegue(withIdentifier: "ShowResults", sender: self)
+    })
 
     dismiss(animated: true)
   }
@@ -94,4 +120,61 @@ extension ViewController: UIImagePickerControllerDelegate {
 
 // MARK: - UINavigationControllerDelegate
 extension ViewController: UINavigationControllerDelegate {
+}
+
+// Networking calls
+extension ViewController {
+  func upload(image: UIImage,
+              progressCompletion: @escaping (_ percent: Float) -> Void,
+              completion: @escaping (_ tags: [String], _ colors: [PhotoColor]) -> Void) {
+    guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
+      print("Could not get JPEG representation of UIImage")
+      return
+    }
+    
+    Alamofire.upload(
+      multipartFormData: { multipartFormData in
+        multipartFormData.append(imageData,
+                                 withName: "imagefile",
+                                 fileName: "image.jpg",
+                                 mimeType: "image/jpeg")
+    },
+      to: "http://api.imagga.com/v1/content",
+      headers: ["Authorization": "Basic YWNjXzY0MmRkMjRmY2NhYjBiZjo5NzM2YjllNjIzOGU2OWI0YjY5N2Q3NjFmYzVjMWEzYg=="],
+      encodingCompletion: { encodingResult in
+        switch encodingResult {
+        case .success(let upload, _, _):
+          upload.uploadProgress { progress in
+            progressCompletion(Float(progress.fractionCompleted))
+          }
+          upload.validate()
+          upload.responseJSON { response in
+            // 1.
+            guard response.result.isSuccess else {
+              print("Error while uploading file: \(response.result.error)")
+              completion([String](), [PhotoColor]())
+              return
+            }
+            
+            // 2.
+            guard let responseJSON = response.result.value as? [String: Any],
+              let uploadedFiles = responseJSON["uploaded"] as? [[String: Any]],
+              let firstFile = uploadedFiles.first,
+              let firstFileID = firstFile["id"] as? String else {
+                print("Invalid information received from service")
+                completion([String](), [PhotoColor]())
+                return
+            }
+            
+            print("Content uploaded with ID: \(firstFileID)")
+            
+            // 3.
+            completion([String](), [PhotoColor]())
+          }
+        case .failure(let encodingError):
+          print(encodingError)
+        }
+    }
+    )
+  }
 }
